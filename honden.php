4328a05e97dacdd6a23433fbb3502fe05d831416
ramilubani <106825937+ramilubani@users.noreply.github.com>
2022-11-22 11:56:23 +0100

    Add files via upload

---
+++ honden.php
@@ -0,0 +1,42 @@
+<?php
+
+class Hond {
+
+    public $gewicht = '5kg';
+    public $kleur = 'wit';
+}
+
+
+                $hond_1 = new Hond();
+                $hond_1->gewicht = '10kg';
+                $hond_1->kleur = 'zwart';
+
+        echo '<pre>';
+        var_dump($hond_1);
+        echo '</pre>';
+
+
+
+
+
+
+                $hond_2 = new Hond();
+                $hond_2->gewicht = '15kg';
+                $hond_2->kleur = 'bruine';
+
+
+        echo '<pre>';
+        var_dump($hond_2);
+        echo '</pre>';
+
+
+
+                $hond_3 = new Hond();
+                $hond_3->gewicht;
+                $hond_3->kleur;
+
+        echo '<pre>';
+        var_dump($hond_3);
+        echo '</pre>';
+
+
